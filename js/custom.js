7b3d23ac74b58b91aab648b65b7e7c73f2188dfa
hzp2333 <1450169683@qq.com>
2024-10-26 13:46:01 +0800

    update-hugo

--- js/custom.js
+++ js/custom.js
@@ -1,49 +1,4 @@
-/* 返回随机颜色 */
-function randomColor() {
-	return "rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")";
-}
 
-/* 鼠标点击文字特效 */
-var a_idx = 0;
-var a_click = 1;
-var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善");
-jQuery(document).ready(function($) {
-    $("body").click(function(e) {
-		/* 点击频率，点击几次就换文字 */
-		var frequency = 2;
-		if (a_click % frequency === 0) {
-			
-			var $i = $("<span/>").text(a[a_idx]);
-			a_idx = (a_idx + 1) % a.length;
-			var x = e.pageX,
-			y = e.pageY;
-			$i.css({
-				"z-index": 9999,
-				"top": y - 20,
-				"left": x,
-				"position": "absolute",
-				"font-weight": "bold",
-				"color": randomColor(),
-				"-webkit-user-select": "none",
-				"-moz-user-select": "none",
-				"-ms-user-select": "none",
-				"user-select": "none"
-			});
-			$("body").append($i);
-			$i.animate({
-				"top": y - 180,
-				"opacity": 0
-			},
-			1500,
-			function() {
-				$i.remove();
-			});
-			
-		}
-	a_click ++;
-		
-    });
-});
 
 
 /* 离开当前页面时修改网页标题，回到当前页面时恢复原来标题 */
